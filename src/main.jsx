5473b850bdfd0fbc07d7ab4e1e4655d20529737e
MarFandino84 <104876435+MarFandino84@users.noreply.github.com>
2024-02-28 18:34:19 -0300

    de

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './src/App.jsx'
-import { GlobalStyles } from './src/components/styles/GlobalStyles.js'
+import App from './App.jsx'
+import { GlobalStyles } from './components/styles/GlobalStyles.js'
 import {Provider} from "react-redux"
 import {PersistGate} from "redux-persist/integration/react"
-import {persistor, store} from "./src/store.js"
+import {persistor, store} from "./store.js"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
 
     <Provider store={store}>
      <PersistGate persistor={persistor}>
@@ -17,5 +16,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
     </PersistGate>
     </Provider>
-     </React.StrictMode>,
 )
